a26f299c2ac2920467147fe91ff11b9b32840d71
Леонид Каретников <poczta130@gmail.com>
2022-12-11 01:08:04 +0000

    index

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
 
+from flask import Flask, render_template, request
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'dupa'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 
 
 if __name__ == '__main__':
